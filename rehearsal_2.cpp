0afe99bac72ccfd2728304bf7a84595ce5290dc4
vVviewVv <viewissara1246@gmail.com>
2020-01-10 15:04:53 +0700

    done

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    float x;
+    float sum = 0;
+
+    cout << "Enter x:";
+    cin >> x;
+
+    while(x != 0){
+        if(x > 0){
+            sum += x;
+            cout << "Enter x:";
+            cin >> x;
+        }else{
+            cout << "Enter x:";
+            cin >> x;
+        }
+    }
+        
+        cout << "sum = " << sum;
+
+    return 0;
+}
