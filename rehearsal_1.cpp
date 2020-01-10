15cdfb0e7b6333cb060263be799d2825996633ed
vVviewVv <viewissara1246@gmail.com>
2020-01-10 14:54:25 +0700

    done

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    string name;
+    float G;
+
+
+    cout << "what is your name?:";
+    cin >> name;
+    cout << "what is your GPA?:";
+    cin >> G;
+
+    if(G < 3.50){
+        cout << "Try harder, " << name << "!!!";
+    }else{
+        cout << name << " Inw Jrim Jrim!!!";
+    }
+
+
+ return 0;
+
+}
